09d32f673fecf1438eff640f76f76c4b05fb91da
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2024-10-03 20:47:40 -0300

    inicia a página de agendamentos

---
+++ frontend/javascript/agendamentos.js
@@ -0,0 +1,135 @@
+let selectedDate = null;
+let selectedTime = null;
+
+$(document).ready(function () {
+  const bookedDates = [
+    moment().add(2, "days").format("DD-MM-YYYY"),
+    moment().add(5, "days").format("DD-MM-YYYY"),
+  ];
+
+  $("#calendar").fullCalendar({
+    locale: "pt-br",
+    selectable: true,
+    selectHelper: true,
+    select: function (start) {
+      const newDate = start;
+      const today = moment();
+
+      if (newDate.isBefore(today, "day")) {
+        alert("Não é permitido selecionar uma data anterior a hoje.");
+        return;
+      }
+
+      if (selectedDate) {
+        $("#calendar").fullCalendar("removeEvents", function (event) {
+          return event.title === "Pré-agendado";
+        });
+      }
+
+      const formattedDate = newDate.format("DD-MM-YYYY");
+
+      if (!bookedDates.includes(formattedDate)) {
+        $("#calendar").fullCalendar(
+          "renderEvent",
+          {
+            title: "Pré-agendado",
+            start: newDate,
+            allDay: true,
+            color: "#F39C21",
+          },
+          true
+        );
+
+        selectedDate = formattedDate;
+        updateAvailableTimes(formattedDate);
+        document.getElementById("summary-date").textContent = selectedDate;
+        document.getElementById("summary").style.display = "block";
+      } else {
+        alert("Esta data já está agendada!");
+      }
+    },
+    events: bookedDates.map((date) => ({
+      title: "Agendado",
+      start: date,
+      allDay: true,
+      color: "red",
+    })),
+    dayRender: function (date, cell) {
+      const formattedDate = date.format("DD-MM-YYYY");
+      if (bookedDates.includes(formattedDate)) {
+        cell.css("background-color", "rgba(255, 0, 0, 0.2)");
+      } else {
+        cell.css("background-color", "rgba(0, 255, 0, 0.2)");
+      }
+    },
+    eventClick: function (event) {
+      alert("Você clicou em: " + event.title);
+    },
+  });
+});
+
+function updateProfessionalInfo() {
+  const professionalName = "João Silva";
+  const professionalSpecialty = "Esteticista";
+  const professionalPrice = "80,00";
+
+  document.getElementById("professional-name").textContent = professionalName;
+  document.getElementById("professional-specialty").textContent =
+    professionalSpecialty;
+  document.getElementById("professional-price").textContent = professionalPrice;
+}
+
+function updateAvailableTimes(date) {
+  const timesList = document.getElementById("times-list");
+  timesList.innerHTML = "";
+
+  const availableTimes = ["09:00", "10:00", "11:00", "14:00", "15:00"];
+
+  availableTimes.forEach(function (time) {
+    const button = document.createElement("button");
+    button.className = "time-button";
+    button.textContent = time;
+    button.onclick = function () {
+      selectTime(time);
+    };
+    timesList.appendChild(button);
+  });
+
+  document.getElementById("available-times").style.display = "block";
+}
+
+function selectTime(time) {
+  const buttons = document.querySelectorAll(".time-button");
+  buttons.forEach((button) => button.classList.remove("selected-time"));
+  event.target.classList.add("selected-time");
+  selectedTime = time;
+
+  document.getElementById("summary-date").textContent = selectedDate;
+  document.getElementById("summary-time").textContent = selectedTime;
+  document.getElementById("summary").style.display = "block";
+  document.getElementById("finalize-button").style.display = "block";
+}
+
+document.getElementById("finalize-button").onclick = function () {
+  window.location.href = "./confirmacaoagendamento.html";
+};
+
+document.getElementById("closeModal").onclick = function () {
+  document.getElementById("eventModal").style.display = "none";
+};
+
+document.getElementById("addEventButton").onclick = function () {
+  const title = document.getElementById("eventTitle").value;
+  if (title) {
+    $("#calendar").fullCalendar(
+      "renderEvent",
+      {
+        title: title,
+        start: selectedDate,
+        allDay: true,
+      },
+      true
+    );
+    document.getElementById("eventModal").style.display = "none";
+  }
+};
---
+++ frontend/javascript/servicos.js
@@ -0,0 +1,110 @@
+let total = 0;
+const services = []; 
+const servicos = [
+  { categoria: 'Cabeleireiro', nome: 'Corte Feminino', preco: 65 },
+  { categoria: 'Cabeleireiro', nome: 'Corte Masculino', preco: 45 },
+  { categoria: 'Cabeleireiro', nome: 'Luzes', preco: 200 },
+  { categoria: 'Cabeleireiro', nome: 'Escova', preco: 40 },
+  { categoria: 'Cabeleireiro', nome: 'Barba / Design de Barba', preco: 40 },
+  { categoria: 'Manicure e Pedicure', nome: 'Manicure', preco: 23 },
+  { categoria: 'Manicure e Pedicure', nome: 'Esmaltação (Cortar + Lixar + Esmaltar)', preco: 13 },
+  { categoria: 'Manicure e Pedicure', nome: 'Francesinha ou Inglesinha', preco: 5 },
+  { categoria: 'Manicure e Pedicure', nome: 'Pedicure + Manicure', preco: 45 },
+  { categoria: 'Manicure e Pedicure', nome: 'Pedicure', preco: 23 },
+  { categoria: 'Estética', nome: 'Drenagem Linfática Corporal Manual (por área)', preco: 65 },
+  { categoria: 'Estética', nome: 'Limpeza de Pele', preco: 85 },
+  { categoria: 'Estética', nome: 'Reflexologia Podal', preco: 85 },
+  { categoria: 'Estética', nome: 'Revitalização Facial', preco: 85 },
+  { categoria: 'Estética', nome: 'Ventosaterapia (Sessão 40 minutos)', preco: 50 },
+  { categoria: 'Depilação com Cera', nome: 'Depilação Feminina - 1/2 Pernas', preco: 30 },
+  { categoria: 'Depilação com Cera', nome: 'Axilias', preco: 24 },
+  { categoria: 'Depilação com Cera', nome: 'Virilha Simples (Contorno)', preco: 28 },
+  { categoria: 'Depilação com Cera', nome: 'Pernas Completas', preco: 55 },
+  { categoria: 'Depilação com Cera', nome: 'Coxas', preco: 40 }
+];
+
+function addService(preco, nome) {
+  total += preco; 
+  services.push({ nome: nome, preco: preco }); 
+  updateUI(); 
+}
+
+function removeService(preco, nome) {
+  total -= preco; 
+  const index = services.findIndex(service => service.nome === nome && service.preco === preco); 
+  if (index > -1) {
+    services.splice(index, 1); 
+  }
+  updateUI(); 
+}
+
+function updateUI() {
+  document.getElementById("total-value").innerText = total.toFixed(2).replace('.', ','); 
+  const servicesList = document.getElementById("services-list");
+  servicesList.innerHTML = ""; 
+  services.forEach(service => {
+    const listItem = document.createElement("li");
+    listItem.innerText = `${service.nome} - R$ ${service.preco.toFixed(2).replace('.', ',')}`; 
+    servicesList.appendChild(listItem); 
+  });
+}
+
+function toggleDropdown(category) {
+  const content = document.getElementById(category);
+  content.style.display = content.style.display === "block" ? "none" : "block";
+}
+
+function generateHTML() {
+  const container = document.getElementById('servicos-container');
+  const categorias = [...new Set(servicos.map(servico => servico.categoria))];
+
+  categorias.forEach(categoria => {
+    const categoryDiv = document.createElement('div');
+    categoryDiv.classList.add('category');
+
+    const button = document.createElement('button');
+    button.classList.add('category-btn');
+    button.setAttribute('onclick', `toggleDropdown('${categoria}')`);
+    button.innerHTML = `${categoria} <span class="arrow">▾</span>`;
+    categoryDiv.appendChild(button);
+
+    const contentDiv = document.createElement('div');
+    contentDiv.id = categoria;
+    contentDiv.classList.add('category-content');
+    contentDiv.style.display = 'none';
+
+    const ul = document.createElement('ul');
+    ul.classList.add('subcategory');
+
+    servicos.filter(servico => servico.categoria === categoria).forEach(servico => {
+      const li = document.createElement('li');
+      li.style.display = 'flex'; 
+      li.style.justifyContent = 'space-between'; 
+      li.style.alignItems = 'center'; 
+
+      li.innerHTML = `
+        <span style="flex: 1; text-align: left;">${servico.nome}</span>
+        <span style="flex: 1; text-align: center;">R$ ${servico.preco.toFixed(2).replace('.', ',')}</span>
+        <span style="flex: 1; text-align: right;">
+          <button class="remove-button" onclick="removeService(${servico.preco}, '${servico.nome}')">-</button>
+          <button class="add-button" onclick="addService(${servico.preco}, '${servico.nome}')">+</button>
+        </span>
+      `;
+      ul.appendChild(li);
+    });
+
+    contentDiv.appendChild(ul);
+    categoryDiv.appendChild(contentDiv);
+    container.appendChild(categoryDiv);
+  });
+}
+
+function cancelarAgendamento() {
+    total = 0; 
+    services.length = 0; 
+    updateUI(); 
+}
+
+document.addEventListener('DOMContentLoaded', generateHTML);
+
+
