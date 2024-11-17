87ba11015e74b0855d4a804e6cec20e62ca6c7fa
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2024-11-16 22:01:12 -0300

    deixa telas responsivas

--- Frontend/funcionario/javascript/cadastro-cliente.js
+++ Frontend/funcionario/javascript/cadastro-cliente.js
@@ -1,3 +1,8 @@
+function toggleMenu() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = sidebar.style.display === 'block' ? 'none' : 'block';
+  }
+
 document.addEventListener('DOMContentLoaded', function () {
     const alunoSim = document.getElementById('alunoSim');
     const alunoNao = document.getElementById('alunoNao');
---
+++ Frontend/funcionario/javascript/clientes.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = sidebar.style.display === 'block' ? 'none' : 'block';
+  }
