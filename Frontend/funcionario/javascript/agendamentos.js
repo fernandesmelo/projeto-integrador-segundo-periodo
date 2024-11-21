b14a3d6f88fdfd2c437d36ffd09771584e1f7ffa
NataliaBento <nataliabento97@gmail.com>
2024-11-21 10:24:51 -0300

    feat: adiciona campo de escolha de funcionario

--- Frontend/funcionario/javascript/agendamentos.js
+++ Frontend/funcionario/javascript/agendamentos.js
@@ -2,3 +2,11 @@ function toggleMenu() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = sidebar.style.display === 'block' ? 'none' : 'block';
   }
+
+  // CAPTURA O FUNCIONÁRIO NA TELA DE AGENDAMENTOS
+document.querySelectorAll('.form-control').forEach(select => {
+  select.addEventListener('change', function () {
+    const funcionarioSelecionado = this.options[this.selectedIndex].text;
+    console.log('Funcionário selecionado:', funcionarioSelecionado);
+  });
+});
