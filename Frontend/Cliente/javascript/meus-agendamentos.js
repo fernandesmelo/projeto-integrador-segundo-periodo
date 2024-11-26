27d5d276d694cdc9c4116cc0a9dc05c7c7976e80
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2024-11-26 16:41:04 -0300

    inicia página de meus agendamentos

---
+++ Frontend/Cliente/javascript/meus-agendamentos.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = sidebar.style.display === "block" ? "none" : "block";
+  }
+  
+  document.querySelectorAll(".form-control").forEach((select) => {
+    select.addEventListener("change", function () {
+      const funcionarioSelecionado = this.options[this.selectedIndex].text;
+      console.log("Funcionário selecionado:", funcionarioSelecionado);
+    });
+  });
+  
