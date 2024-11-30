51c8f1570cdc06a4bdcb2fe81722d8dfda72b40d
Matheus Bezerra <matheusbzrr3@gmail.com>
2024-11-30 02:44:22 -0300

    integrando meus agendamentos

--- Frontend/Cliente/javascript/meus-agendamentos.js
+++ Frontend/Cliente/javascript/meus-agendamentos.js
@@ -10,3 +10,64 @@ function toggleMenu() {
     });
   });
   
+
+  function carregarAgendamentos() {
+    const cliente = JSON.parse(localStorage.getItem('cliente'));
+    console.log(cliente);
+
+    fetch(`http://localhost:8080/salaosenac/agendamentos/cliente/${cliente.idCliente}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // Extrai apenas o primeiro elemento da resposta, que é o array com os agendamentos
+        const agendamentos = data[0]; 
+  
+        // Verifica se há agendamentos retornados
+        if (!agendamentos || agendamentos.length === 0) {
+          console.warn('Nenhum agendamento encontrado.');
+          return;
+        }
+  
+        const contentDiv = document.querySelector('.content .row');
+        contentDiv.innerHTML = ''; // Limpa o conteúdo anterior
+  
+        agendamentos.forEach((agendamento, index) => {
+          // Cria dinamicamente cada card
+          const cardHtml = `
+          <div class="col-md-4">
+            <div class="card">
+              <div class="card-header">
+                <span>Cliente: ${agendamento.nomeCliente}</span>
+              </div>
+              <div class="card-body">
+                <div>
+                  <strong>Data:</strong> ${new Date(agendamento.data).toLocaleDateString()}<br>
+                  <strong>Horário:</strong> ${agendamento.horario}<br>
+                  <strong>Profissional:</strong> ${agendamento.nomeFuncionario}<br>
+                  <strong>Serviços:</strong>
+                  <ul>
+                    <li>${agendamento.servicosAssociados}</li>
+                  </ul>
+                  <strong>Total:</strong> R$ ${agendamento.valorTotal}
+                </div>
+                <div style="margin-top: 30px;">
+                  <a href="#" class="btn-secondary-action">Reagendar</a>
+                  <a href="#" class="btn-action">Cancelar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          `;
+          contentDiv.insertAdjacentHTML('beforeend', cardHtml);
+        });
+      })
+      .catch((error) => {
+        console.error('Erro ao carregar agendamentos:', error);
+      });
+  }
+
+
+
+
+// Chamada da função ao carregar a página
+document.addEventListener('DOMContentLoaded', carregarAgendamentos);
